00d9aa04bcfc688d981575c8ebcd2445110725ba
Ray Lai <rayymlai@gmail.com>
2016-06-28 15:05:07 -0700

    remove api/

--- api/api_project.js
+++
@@ -1,1 +0,0 @@
-define({  "name": "QuindarPlatform",  "version": "0.1.0",  "description": "Quindar platform consists of data services (REST API) and webSocket services for real time data streaming.",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2016-06-15T20:10:24.015Z",    "url": "http://apidocjs.com",    "version": "0.16.1"  }});
