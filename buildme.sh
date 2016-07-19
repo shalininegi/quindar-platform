5663041e1069940a1193e561dabc672d822b0b8c
Ray Lai <rayymlai@gmail.com>
2016-07-19 11:29:39 -0700

    fix /log auto-delete issue

--- buildme.sh
+++ buildme.sh
@@ -2,14 +2,3 @@
 echo "Building NodeJS dependencies for Quindar platform"
 echo "...installing npm modules from package.json"
 npm install
-
-echo "...preparing logging"
-if [ -d "log" ]; then
-   echo "...cleaning up /log subfolder"
-   rm -fr log
-else
-   echo "...creating /log subfolder"
-   mkdir log
-   touch log/.verifyMeSystemAccess
-fi
-echo "...complete"
