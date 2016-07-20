94d69185efb1df7b4a44458f793798e4b48a40f8
Ray lai <rayymlai@gmail.com>
2016-07-20 09:50:02 -0700

    update timetoliveIndex.sh comment

--- admin/db/timetoliveIndex.sh
+++ admin/db/timetoliveIndex.sh
@@ -2,7 +2,8 @@
 # Program: timetoliveIndex.sh
 # Purpose: add time to live index so that MongoDB can auto-purge old data (> 3 months)
 # Author:  Ray Lai
-# Updated: Jun 30, 2016
+# Updated: Jul 20, 2016
+# Example: ./timetoliveIndex.sh myusername mypassword
 # Remark:  running this script will auto purge old records after 6 months (15552000 seconds)
 #
 if [ $# -eq 0 ]; then
@@ -14,11 +15,16 @@ else
   DUMPDIR=/mnt/data/tmp/databaseDump
   DBHOST=data01.audacy.space
   DBPORT=11001
-  
   mongo -u $1 -p $2 --authenticationDatabase $MONGOADMINDB --host $DBHOST --port $DBPORT << leftcurlybracket
   use telemetry
-  db.postion.dropIndex("autoPurge");
+  db.attitude.dropIndex("autoPurge");
+  db.attitude.createIndex({ "createdAt": 1 }, { background: true, name: "autoPurge", expireAfterSeconds: 15552000 });
+  db.position.dropIndex("autoPurge");
   db.position.createIndex({ "createdAt": 1 }, { background: true, name: "autoPurge", expireAfterSeconds: 15552000 });
+  db.vehicle.dropIndex("autoPurge");
+  db.vehicle.createIndex({ "createdAt": 1 }, { background: true, name: "autoPurge", expireAfterSeconds: 15552000 });
+  db.orbit.dropIndex("autoPurge");
+  db.orbit.createIndex({ "createdAt": 1 }, { background: true, name: "autoPurge", expireAfterSeconds: 15552000 });
   exit
 leftcurlybracket
 fi
